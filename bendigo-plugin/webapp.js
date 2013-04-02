d05704f3e8172b097d167d4ef5805e018c4b43a0
Peter Braden <peterbraden@peterbraden.co.uk>
2013-04-02 12:52:07 -0700

    Testing new plugin style

--- bendigo-plugin/webapp.js
+++ bendigo-plugin/webapp.js
@@ -1,12 +1,34 @@
 module.exports = function(ctx, cb){
 
+  console.log(ctx);
+  ctx.route.get('/bendigo-dash-frame.html', function(req, res, next){ 
+    var Job = res.locals.models.Job;
 
-  //ctx.routes.get('/bendigo-dash.json', function(req, res, next){});
+    Job.find()
+      .sort({'finished_timestamp': -1})
+      .where('finished_timestamp').ne(null)
+      .where('archived_timestamp', null)
+      .where('type').in(['TEST_ONLY','TEST_AND_DEPLOY'])
+      .limit(20)
+      .lean(true)
+      .populate("_owner")
+      .exec(function(err,results){
+        console.log(results);
+        var out = "<h1>Recent Jobs</h1>"
+        results.forEach(function(r){
+          out += "<div><h2 style='background-color:"
+          out += "" +  ((r.test_exitcode == 0) ? '#0f0' : '#f00') + ";'>" 
+          out += r.repo_url + "</h2></div>"
+        })
+
+        // Proof of concept hackery
+        res.send(out); 
+      })
+  });
   
 
-  ctx.registerBlock("LoggedOutFillContent", function(ctx){
-    return "<h1>Hello, world</h1>"
-  
+  ctx.registerBlock("LoggedOutFillContent", function(context){
+    return "<iframe src='/bendigo-dash-frame.html' style='width:100%; border:none;'>Loading...</iframe>"
   })
 
   cb(null);
