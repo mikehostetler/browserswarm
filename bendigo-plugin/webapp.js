1f9bb0c3aa211911cc1773e86670e165cfdac95a
Peter Braden <peterbraden@peterbraden.co.uk>
2013-05-29 15:00:01 -0700

    WIP on frameworks styling etc

--- bendigo-plugin/webapp.js
+++ bendigo-plugin/webapp.js
@@ -1,6 +1,32 @@
 var swig = require('swig')
+  , frameworks = require("./frameworks.json")
+  , frameworksObj = {}
+
+frameworks.forEach(function(x){
+  if (x.id)
+  frameworksObj[x.id] = x;
+})
 
 module.exports = function(ctx, cb){
+
+  ctx.route.get("/frameworks.json", function(req, res, next){
+    var f = frameworks.filter(function(x){return !!x.id})  
+    res.send(f)
+  })
+
+  ctx.route.get("/framework/results/:id", function(req, res, next){
+
+    var framework = frameworksObj[req.params.id]
+
+    if(!framework) return next();
+    
+    console.log("!!>", framework);
+
+    res.send(swig.compileFile(__dirname + "/framework.html").render({
+        "id" : framework.id
+      , "name" : framework.name
+    }))
+  })
 
   ctx.registerBlock("Nav", function(context, cb){
     cb(null, "&nbsp;");
