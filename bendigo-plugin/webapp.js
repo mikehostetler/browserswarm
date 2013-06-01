881aec9fdc57ab8ae967dc44e077e92913e69bb8
Peter Braden <peterbraden@peterbraden.co.uk>
2013-05-31 18:34:51 -0700

    generate framework page dynamically

--- bendigo-plugin/webapp.js
+++ bendigo-plugin/webapp.js
@@ -28,9 +28,44 @@ module.exports = function(ctx, cb){
       console.log("!!!", arguments);
       // TODO : Filter jobs for browsers
       // TODO : Marshall into usable json
+      
+      var mockData = [{
+        id : "latest_build"
+      , "chrome24" : "supported"
+      , "chrome25" : "supported"
+      , "firefox18" : "supported"
+      , "firefox19" : "supported"
+      , "ie6" : "not"
+      , "ie7" : "supported"
+      , "ie8" : "supported"
+      , "ie9" : "supported"
+      , "ie10" : "supported"
+      , "safari5" : "unknown"
+      , "safari6" : "supported"
+      , "opera12" : "supported"
+      , "opera13" : "supported"
+      }]
+
       res.send(swig.compileFile(__dirname + "/framework.html").render({
           "id" : framework.id
         , "name" : framework.name
+        , "results" : mockData
+        , "repo" : "peterbraden/jquery"
+        , "browsers" : [
+          {id : "chrome24", name : "Chrome 24", ico : "chrome"}
+        , {id : "chrome25", name : "Chrome 25", ico : "chrome"}
+        , {id : "firefox18", name : "Firefox 18", ico : "firefox"}
+        , {id : "firefox19", name : "Firefox 19", ico : "firefox"}
+        , {id : "ie6", name : "IE 6", ico : "ie-6"}
+        , {id : "ie7", name : "IE 7", ico : "ie-8"}
+        , {id : "ie8", name : "IE 8", ico : "ie-8"}
+        , {id : "ie9", name : "IE 9", ico : "ie"}
+        , {id : "ie10", name : "IE 10", ico : "ie-10"}
+        , {id : "safari5", name : "Safari 5.1", ico : "safari"}
+        , {id : "safari6", name : "Safari 6.0", ico : "safari"}
+        , {id : "opera12", name : "Opera 12.0", ico : "opera"}
+        , {id : "opera13", name : "Opera 13.0", ico : "opera"}
+        ]
       }))
     })
   })
