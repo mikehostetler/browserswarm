64f31a38ee075412e165938b0c7a99b1212566b6
Peter Braden <peterbraden@peterbraden.co.uk>
2013-06-01 11:50:21 -0700

    frameworks page

--- bendigo-plugin/webapp.js
+++ bendigo-plugin/webapp.js
@@ -7,6 +7,22 @@ frameworks.forEach(function(x){
   frameworksObj[x.id] = x;
 })
 
+var browsers = [
+  {id : "chrome24", name : "Chrome 24", ico : "chrome"}
+, {id : "chrome25", name : "Chrome 25", ico : "chrome"}
+, {id : "firefox18", name : "Firefox 18", ico : "firefox"}
+, {id : "firefox19", name : "Firefox 19", ico : "firefox"}
+, {id : "ie6", name : "IE 6", ico : "ie-6"}
+, {id : "ie7", name : "IE 7", ico : "ie-8"}
+, {id : "ie8", name : "IE 8", ico : "ie-8"}
+, {id : "ie9", name : "IE 9", ico : "ie"}
+, {id : "ie10", name : "IE 10", ico : "ie-10"}
+, {id : "safari5", name : "Safari 5.1", ico : "safari"}
+, {id : "safari6", name : "Safari 6.0", ico : "safari"}
+, {id : "opera12", name : "Opera 12.0", ico : "opera"}
+, {id : "opera13", name : "Opera 13.0", ico : "opera"}
+]
+
 module.exports = function(ctx, cb){
 
   ctx.route.get("/frameworks.json", function(req, res, next){
@@ -23,49 +39,31 @@ module.exports = function(ctx, cb){
     var jobs = res.locals.models.Job
                  .find({repo_url: framework.repo_url})
                  .limit(10)
-                 .exec(function(){
+                 .exec(function(err, jobs){
 
-      console.log("!!!", arguments);
       // TODO : Filter jobs for browsers
       // TODO : Marshall into usable json
-      
-      var mockData = [{
-        id : "latest_build"
-      , "chrome24" : "supported"
-      , "chrome25" : "supported"
-      , "firefox18" : "supported"
-      , "firefox19" : "supported"
-      , "ie6" : "not"
-      , "ie7" : "supported"
-      , "ie8" : "supported"
-      , "ie9" : "supported"
-      , "ie10" : "supported"
-      , "safari5" : "unknown"
-      , "safari6" : "supported"
-      , "opera12" : "supported"
-      , "opera13" : "supported"
-      }]
+     
+      // TODO : Mock Data:
+      var mockData = []
+      for (x in jobs){ 
+        var job = jobs[x]
+        console.log("Job: " , job._id)
+        var j = {id : job._id}
+        j.id_short  = (job._id + "").slice(0,9)
+        for (var i in browsers){
+         j[browsers[i].id] = "unknown"
+        }
+        mockData.push(j)
+      }
 
       res.send(swig.compileFile(__dirname + "/framework.html").render({
           "id" : framework.id
         , "name" : framework.name
+        , "src" : framework.src
         , "results" : mockData
         , "repo" : framework.repo
-        , "browsers" : [
-          {id : "chrome24", name : "Chrome 24", ico : "chrome"}
-        , {id : "chrome25", name : "Chrome 25", ico : "chrome"}
-        , {id : "firefox18", name : "Firefox 18", ico : "firefox"}
-        , {id : "firefox19", name : "Firefox 19", ico : "firefox"}
-        , {id : "ie6", name : "IE 6", ico : "ie-6"}
-        , {id : "ie7", name : "IE 7", ico : "ie-8"}
-        , {id : "ie8", name : "IE 8", ico : "ie-8"}
-        , {id : "ie9", name : "IE 9", ico : "ie"}
-        , {id : "ie10", name : "IE 10", ico : "ie-10"}
-        , {id : "safari5", name : "Safari 5.1", ico : "safari"}
-        , {id : "safari6", name : "Safari 6.0", ico : "safari"}
-        , {id : "opera12", name : "Opera 12.0", ico : "opera"}
-        , {id : "opera13", name : "Opera 13.0", ico : "opera"}
-        ]
+        , "browsers" : browsers
       }))
     })
   })
