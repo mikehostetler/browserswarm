c751c5ec46d6b1daee23b654d26b5dce6d726a5e
Peter Braden <peterbraden@peterbraden.co.uk>
2013-06-05 10:15:15 -0700

    jobs filter

--- bendigo-plugin/webapp.js
+++ bendigo-plugin/webapp.js
@@ -8,18 +8,18 @@ frameworks.forEach(function(x){
 })
 
 var browsers = [
-  {id : "chrome24", name : "Chrome 24", ico : "chrome"}
-, {id : "chrome25", name : "Chrome 25", ico : "chrome"}
-, {id : "firefox18", name : "Firefox 18", ico : "firefox"}
-, {id : "firefox19", name : "Firefox 19", ico : "firefox"}
-, {id : "ie6", name : "IE 6", ico : "ie-6"}
-, {id : "ie7", name : "IE 7", ico : "ie-8"}
-, {id : "ie8", name : "IE 8", ico : "ie-8"}
-, {id : "ie9", name : "IE 9", ico : "ie"}
-, {id : "ie10", name : "IE 10", ico : "ie-10"}
-, {id : "safari5", name : "Safari 5.1", ico : "safari"}
-, {id : "safari6", name : "Safari 6.0", ico : "safari"}
-, {id : "opera12", name : "Opera 12.0", ico : "opera"}
+  {id : "chrome24", name : "Chrome 26", ico : "chrome", bs : "win-chrome-26.0"}
+, {id : "chrome27", name : "Chrome 27", ico : "chrome", bs : "win-chrome-27.0"}
+, {id : "firefox18", name : "Firefox 18", ico : "firefox", bs : "win-firefox-18.0"}
+, {id : "firefox19", name : "Firefox 19", ico : "firefox", bs : "win-firefox-19.0"}
+, {id : "ie6", name : "IE 6", ico : "ie-6", bs : "win-ie-6.0"}
+, {id : "ie7", name : "IE 7", ico : "ie-8", bs : "win-ie-7.0"}
+, {id : "ie8", name : "IE 8", ico : "ie-8", bs : "win-ie-8.0"}
+, {id : "ie9", name : "IE 9", ico : "ie", bs : "win-ie-9.0"}
+, {id : "ie10", name : "IE 10", ico : "ie-10", bs : "win-ie-10.0"}
+, {id : "safari5", name : "Safari 5.1", ico : "safari", bs: "mac-safari-5.1"}
+, {id : "safari6", name : "Safari 6.0", ico : "safari", bs :"mac-safari-6.0"}
+, {id : "opera12", name : "Opera 12.10", ico : "opera", bs: "win-opera-12.10"}
 , {id : "opera13", name : "Opera 13.0", ico : "opera"}
 ]
 
@@ -46,32 +46,48 @@ module.exports = function(ctx, cb){
                  .where('finished_timestamp').ne(null)
                 // Only jobs which have not been archived
                  .where('archived_timestamp', null)
+                // Only jobs with results stored
+                 .where('tasks').ne(null)
                  .limit(10)
                  .populate('_owner')
                  .lean(true)
                  .exec(function(err, jobs){
 
-      // TODO : Filter jobs for browsers
-      // TODO : Marshall into usable json
-     
-      // TODO : Mock Data:
-      var mockData = []
-      for (x in jobs){ 
+      var jobData = []
+      for (x in jobs){
         var job = jobs[x]
-        console.log("Job: " , job._id)
+
+        if (!job.tasks.length)
+          continue;
+
         var j = {id : job._id}
         j.id_short  = (job._id + "").slice(0,9)
+
         for (var i in browsers){
-         j[browsers[i].id] = "unknown"
+
+          j[browsers[i].id] = "unknown"
+
+          if (browsers[i].bs){
+            for (var z =0; z< job.tasks.length; z++){
+              var brows = job.tasks[z].id;
+
+              if (browsers[i].bs == brows){
+                // Browser in job results:
+                j[browsers[i].id] = (job.tasks[z].failed == 0) ? "supported" : "not";
+              }
+            }
+          }
+
         }
-        mockData.push(j)
+
+        jobData.push(j)
       }
 
       res.send(swig.compileFile(__dirname + "/framework.html").render({
           "id" : framework.id
         , "name" : framework.name
         , "src" : framework.src
-        , "results" : mockData
+        , "results" : jobData
         , "repo" : framework.repo
         , "browsers" : browsers
       }))
@@ -97,6 +113,8 @@ module.exports = function(ctx, cb){
   })
 
   ctx.registerBlock("JobPagePreCols", function(context, fn){
+    console.log("Job Page: need job id", Object.keys(context))
+    console.dir(context)
     var tmpl = swig.compileFile(__dirname  + "/JobPagePreCols.html")
       , job = context.models.Job
                  .find()
