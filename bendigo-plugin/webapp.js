78c5b40319180a26e91127ded8e15d41739bec9e
Mike Hostetler <mike.hostetler@gmail.com>
2013-09-10 20:02:52 -0500

    Finished upgrading the job results page

--- bendigo-plugin/webapp.js
+++ bendigo-plugin/webapp.js
@@ -1,8 +1,18 @@
+var BASE_PATH = "../node_modules/strider/lib/"
+
 var swig = require('swig')
 	, _ = require('underscore')
+  , mongoose = require('mongoose')
+  , crypto = require('crypto')
+  , Step = require('step')
   , frameworks = require("./frameworks.json")
   , frameworksObj = {}
   , repoFrameworks = {}
+  , filter = require(BASE_PATH + 'ansi')
+  , humane = require(BASE_PATH + 'humane')
+  , Job = require(BASE_PATH + 'models').Job
+  , User = require(BASE_PATH + 'models').User
+	;
 
 frameworks.forEach(function(x){
   if (x.id)
@@ -37,10 +47,122 @@ module.exports = function(ctx, cb){
     res.send(f)
   })
 
-  ctx.route.get("/:org/:repo/job/:job_id", function(req, res, next){
-		console.log("One Two Three!");
-    res.send("bla")
+  ctx.route.get("/:org/:repo/result/:job_id", function(req, res, next){
+		res.statusCode = 200;
+		var org = req.params.org;
+		var repo = req.params.repo;
+		var job_id = req.params.job_id;
+		var repo_url = "https://github.com/" + org + "/" + repo;
+
+		// Ignore if can't parse as ObjectID
+		try {
+			job_id = mongoose.Types.ObjectId(job_id);
+		} catch(e) {
+			res.statusCode = 400;
+			return res.end("job_id must be a valid ObjectId");
+		}
+
+		Step(
+			function getRepoConfig() {
+				lookup(repo_url, this);
+			},
+			function runQueries(err, repo_config){
+				if (err || !repo_config) {
+					res.statusCode = 500;
+					res.end("you must configure " + repo_url + " before you can use it");
+					return;
+				}
+				this.repo_config = repo_config;
+				console.log("Querying for job id: " + job_id);
+				Job.findById(job_id).populate("_owner").lean(true).exec(this.parallel());
+
+				console.log("Querying for last 20 jobs for " + repo_url);
+				Job.find()
+					.sort({'finished_timestamp': -1})
+					.where('finished_timestamp').ne(null)
+					.where('repo_url',this.repo_config.url)
+					.where('archived_timestamp', null)
+					.where('type').in(['TEST_ONLY','TEST_AND_DEPLOY'])
+					.limit(20)
+					.populate("_owner")
+					.lean(true)
+					.exec(this.parallel());
+			},
+			function processAndRender(err, results_detail, results) {
+				if (err) throw err;
+
+				_.each(results, function(job) {
+					job.id = job._id.toString();
+					job.duration = Math.round((job.finished_timestamp - job.created_timestamp)/1000);
+					// Some jobs never finish, to due a crash or other error condition
+					if (!results_detail.finished_timestamp) {
+						results_detail.finished_at = "Did not finish"
+					} else {
+						results_detail.finished_at = humane.humaneDate(results_detail.finished_timestamp);
+					}
+					if (job.github_commit_info !== undefined && job.github_commit_info.id !== undefined) {
+						job.triggered_by_commit = true;
+						job.gravatar_url = 'https://secure.gravatar.com/avatar/'
+							+ crypto.createHash('md5').update(job.github_commit_info.author.email).digest("hex")
+							+ '.jpg?' + 'd=' + encodeURIComponent('identicon');
+						if (job.github_commit_info.author.username != undefined) {
+							job.committer = job.github_commit_info.author.username;
+							job.committer_is_username = true;
+						} else {
+							job.committer = job.github_commit_info.author.name;
+							job.committer_is_username = false;
+						}
+					}
+					job.url = "/" + org + "/" + repo + "/job/" + job.id;
+				});
+
+				// if results_detail did not return, that means this is not a valid job id
+				if (!results_detail) {
+					res.render(404, 'invalid job id');
+				} else {
+
+					results_detail.duration = Math.round((results_detail.finished_timestamp - results_detail.created_timestamp)/1000);
+					results_detail.finished_at = humane.humaneDate(results_detail.finished_timestamp);
+					results_detail.id = results_detail._id.toString();
+
+					var triggered_by_commit = false;
+					if (results_detail.github_commit_info !== undefined && results_detail.github_commit_info.id !== undefined) {
+						triggered_by_commit = true;
+						results_detail.gravatar_url = 'https://secure.gravatar.com/avatar/' + crypto.createHash('md5').update(results_detail.github_commit_info.author.email).digest("hex") + '.jpg?' + 'd=' + encodeURIComponent('identicon');
+						if (results_detail.github_commit_info.author.username != undefined) {
+							results_detail.committer = results_detail.github_commit_info.author.username;
+							results_detail.committer_is_username = true;
+						} else {
+							results_detail.committer = results_detail.github_commit_info.author.name;
+							results_detail.committer_is_username = false;
+						}
+					}
+
+					// Jobs which have not finished have no output
+					if (!results_detail.stdmerged) {
+						results_detail.output = "[STRIDER] This job has no output."
+					} else {
+						results_detail.output = filter(results_detail.stdmerged);
+					}
+
+					res.render(__dirname + '/job.html',
+						{
+							admin_view: false,
+							jobs: results,
+							results_detail: results_detail,
+							job_id: req.params.job_id,
+							triggered_by_commit: triggered_by_commit,
+							org:org,
+							repo:repo,
+							repo_url:this.repo_config.url,
+							has_prod_deploy_target:this.repo_config.has_prod_deploy_target
+						});
+
+				}
+			}
+		);
 	});
+
   ctx.route.get("/framework/results/:id", function(req, res, next){
 
     var framework = frameworksObj[req.params.id]
@@ -252,6 +374,27 @@ module.exports = function(ctx, cb){
   
   cb(null);
 }
+
+function lookup(case_insensitive_url, cb) {
+  User.findOne({
+      "github_config.url":case_insensitive_url.toLowerCase(),
+    }, function(err, user_obj) {
+    if (err || !user_obj) {
+      console.debug("lookup() - did not find a repo matching %s for any user",
+        case_insensitive_url);
+      return cb("no repo found", null);
+    }
+    var repo = _.find(user_obj.github_config, function(repo_config) {
+      return case_insensitive_url.toLowerCase() == repo_config.url;
+    });
+    if (!repo) {
+      console.error(
+        "lookup() - Error finding matching github_config despite DB query success!");
+      return cb("no repo found", null);
+    }
+    return cb(null, repo);
+  });
+};
 
 function escapeHtml(text) {
 	text = text + "";
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var strider = require('strider')
 strider([".", "node_modules", "node_modules/strider/node_modules"], {
 
 }, function(){
-  console.log("Bendigo is running");
+  console.log("BrowserSwarm is running");
 });
