46c00e2bb1f7344f69ff15c4d25b80201f191365
Mike Hostetler <mike.hostetler@gmail.com>
2013-09-10 21:05:03 -0500

    Tweaking spacing

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var strider = require('strider')
 
-
-strider([".", "node_modules", "node_modules/strider/node_modules"], {
-
-}, function(){
+strider([".", "node_modules", "node_modules/strider/node_modules"], {}, function(){
   console.log("BrowserSwarm is running");
 });
