0e18b3e69abfd2d58b681b6e6dd83058ea6f9005
nicholascloud <me@nicholascloud.com>
2013-09-19 12:12:43 -0500

    dashboard and home are now separate; updated nav

--- browserswarm-plugin/routes/dashboard.js
+++ browserswarm-plugin/routes/dashboard.js
@@ -1,21 +1,26 @@
 'use strict';
 var path = require('path');
 
-var STRIDER_PATH = path.resolve(__dirname, '../../node_modules/strider'),
-  STRIDER_VIEWS_PATH = path.join(STRIDER_PATH, 'views');
+var STRIDER_PATH = path.resolve(__dirname, '../../node_modules/strider');
+
+function view(name) {
+  return path.join(STRIDER_PATH, 'views', name);
+}
 
 module.exports = function (ctx) {
 
   ctx.route.get('/dashboard', function(req, res, next) {
-    return res.render(path.join(STRIDER_VIEWS_PATH, 'index.html')/*, viewData*/);
-
     if (!req.user) {
-      return req.redirect('/');
+      return res.redirect('/');
     }
+
     req.user.get_repo_config_list(function(err, repo_list) {
       if (err) throw err;
-      var viewData = {total_configured_projects:repo_list.length};
-      res.render(path.join(STRIDER_VIEWS_PATH, 'index.html')/*, viewData*/);
+      res.render(view('index.html'), {
+        total_configured_projects: repo_list.length,
+        showHome: false
+      });
     });
+
   });
 };
