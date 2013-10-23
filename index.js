f5e51b3aa4fe1336c86e4a56ef16e0c2d1085212
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-10-23 18:57:52 +0100

    removed all plugins, to be added later gradually

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var strider = require('strider')
 
-var testWorker = false;
-var includePath = [];
-if(testWorker) {
-	includePath = ["node_modules", "node_modules/strider/node_modules"];
-}
-else {
-	includePath = [".", "node_modules", "node_modules/strider/node_modules"];
-}
+// var testWorker = false;
+// var includePath = [];
+// if(testWorker) {
+// 	includePath = ["node_modules", "node_modules/strider/node_modules"];
+// }
+// else {
+var includePath = [".", "node_modules", "node_modules/strider/node_modules"];
+// }
 
-strider(includePath, {}, function(){
+var app = strider(includePath, {}, function(){
 	console.log("BrowserSwarm is running");
 });
+
+/// Ugly hack
+app.stack.unshift({ route: '', handle: require('./cors') });
