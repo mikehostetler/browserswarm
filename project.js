90c7f809da477ae9878b10a4a40fdfc020c6fa09
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-10-29 18:45:16 +0000

    adding access level to project

--- project.js
+++ project.js
@@ -1,5 +1,14 @@
+var extend = require('util')._extend;
+
 exports.get = getProject;
 
 function getProject(req, res, next) {
-  res.send(req.project);
+  var project = req.project;
+  if (project) {
+    project = extend({}, project._doc);
+    if (project.access_level)
+      project.access_level = req.accessLevel;
+  }
+
+  res.send(project);
 }
