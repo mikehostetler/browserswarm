fcb4675020f29cf3ea6d6d1d14d54a28983f285e
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-11-07 09:23:31 +0000

    CORS conf

--- index.js
+++ index.js
@@ -12,7 +12,34 @@ var includePath = [".", "node_modules", "node_modules/strider/node_modules"];
 // }
 
 var config = {
-  cors: 'http://localhost:1337'
+  cors: {
+    origin: 'http://localhost:1337',
+    credentials: true,
+    headers: [
+      'DNT',
+      'X-Mx-ReqToken',
+      'Keep-Alive',
+      'User-Agent',
+      'X-Requested-With',
+      'If-Modified-Since',
+      'Cache-Control',
+      'Content-Type',
+      'Accept',
+      'Accept-Encoding',
+      'Origin',
+      'Referer',
+      'Pragma',
+      'Cookie'
+    ],
+    methods: [
+      'GET',
+      'PUT',
+      'POST',
+      'DELETE',
+      'OPTIONS'
+    ],
+    maxAge: 1728000
+  }
 };
 
 var app = strider(includePath, config, function(){
